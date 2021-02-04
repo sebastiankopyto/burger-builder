b6b7fdb064401108ae6bad14c590c519639e3501
Sebastian Kopyto <sebastian.kopyto@gmail.com>
2021-02-04 21:49:01 +0100

    Burger builder with price and order button

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import classes from './BuildControls.css';
+import BuildControl from './BuildControl/BuildControl';
+
+const controls = [
+  { label: 'Salad', type: 'salad' },
+  { label: 'Bacon', type: 'bacon' },
+  { label: 'Cheese', type: 'cheese' },
+  { label: 'Meat', type: 'meat' },
+];
+
+const buildControls = (props) => (
+  <div className={classes.buildControls}>
+
+    <p>Current price: <strong>{props.price.toFixed(2)}</strong></p>
+
+    { controls.map(control => 
+      <BuildControl 
+        key={control.label} 
+        label={control.label} 
+        added={() => props.ingredientAdded(control.type)}
+        removed={() => props.ingredientRemoved(control.type)}
+        disabled={props.disabled[control.type]} />) }
+
+        <button 
+          className={classes.OrderButton}
+          disabled={!props.purchasable}>ORDER NOW</button>
+  </div>
+);
+
+export default buildControls;
